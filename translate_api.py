68fd0aae5ef8fc61a640bb39160b3a74ca95c097
roper <roper@qq.com>
2023-04-05 10:42:14 +0800

    c

---
+++ translate_api.py
@@ -0,0 +1,12 @@
+from pygtrans import Translate
+import logging
+
+client = Translate()
+
+def translate(text, dest='en', src='auto'):
+    logging.info(f'trans input src:{src}, dest:{dest}')
+    if '' == text:
+        return ''
+    # res = translator.translate(text, dest=dest, src=src)
+    res = client.translate(text, source=src, target=dest)
+    return res.translatedText
